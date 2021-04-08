f7a1d3102711ea24727c698b84458755759d709a
OriolFalip <Oriol.Falip@weidmueller.com>
2021-04-08 11:10:53 +0200

    Adding response

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const http = require('http'); //Import http module
 //Using arrow functions
 const server = http.createServer((req,res)=>{
     console.log('Hello from the server');
+    console.log(req.url, req.method, req.headers);
+
+    //This is the hard way to write the content. In the following lectures, this will be easelly done with Express
+    res.setHeader('Content-type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>My first web page</title></head>');
+    res.write('<body><h1>Hello from the web page</h1></body>');
+    res.write('</html>');
 });  //Our server is now created and when a request is recieved, it executes the requestListener function.
 //However, there is no way we can send it a request, because it is not listening.
 
